001dfb9261784f44ad7f373f2a9152a79ff82979
pch573 <cksgho573@gmail.com>
2021-09-27 13:56:06 +0900

    v-for을 활용한 로그추가, 클래스 바인딩을 활용한 스타일 적용, 게임종료 팝업추가

--- app.js
+++ app.js
@@ -7,41 +7,80 @@ new Vue({
         winner: null,
         lifeOfMe: 3,
         lifeOfCom: 3,
-    },
-    watch: {
-        count: function(newVal){
-            if(newVal ===0){
-              let number = Math.random()
-              if(number < 0.33){
-                this.comChoice = 'scissor'
-              }else if(number <0.66){
-                this.comChoice = 'rock'    
-              }else{
-                this.comChoice = 'paper'
-              }
-            //   가위바위보 승패 결정 
-            if(this.myChoice === this.comChoice) this.winner= 'no one'
-            else if(this.myChoice === 'rock' && this.comChoice ==='scissor')this.winner ='me'
-            else if(this.myChoice === 'scissor' && this.comChoice === 'paper')this.winner ='me'
-            else if(this.myChoice === 'paper' && this.comChoice === 'rock')this.winner ='me'
-            else if(this.myChoice === 'scissor' && this.comChoice === 'rock')this.winner ='com'
-            else if(this.myChoice === 'paper' && this.comChoice ==='scissor')this.winner ='com'
-            else if(this.myChoice === 'rock' && this.comChoice ==='paper')this.winner ='com'
-            else this.winner= 'error'
-  
-            // 몫 차감   
-            if(this.winner=== 'me'){
-                this.lifeOfCom --
-            }
-            else if(this.winner=== 'com'){
-                this.lifeOfMe --
-            }
+        isSelectable: true,
+        logs:[]
+        },
+        watch: {
+            count: function(newVal){
+                if(newVal ===0){
+                let number = Math.random()
+                if(number < 0.33){
+                    this.comChoice = 'scissor'
+                }else if(number <0.66){
+                    this.comChoice = 'rock'    
+                }else{
+                    this.comChoice = 'paper'
+                }
+                //   가위바위보 승패 결정 
+                if(this.myChoice === this.comChoice) this.winner= 'no one'
+                else if(this.myChoice === 'rock' && this.comChoice ==='scissor')this.winner ='me'
+                else if(this.myChoice === 'scissor' && this.comChoice === 'paper')this.winner ='me'
+                else if(this.myChoice === 'paper' && this.comChoice === 'rock')this.winner ='me'
+                else if(this.myChoice === 'scissor' && this.comChoice === 'rock')this.winner ='com'
+                else if(this.myChoice === 'paper' && this.comChoice ==='scissor')this.winner ='com'
+                else if(this.myChoice === 'rock' && this.comChoice ==='paper')this.winner ='com'
+                else this.winner= 'error'
+    
+                // 몫 차감   
+                if(this.winner=== 'me'){
+                     this.lifeOfCom --
+                 }
+                else if(this.winner=== 'com'){
+                    this.lifeOfMe --
+                }   
+                this.count = 3
+                // 버튼이 다시 보이게 됨
+                this.isSelectable = true
 
+                let log = {
+                    message: `You: ${this.myChoice}, Computer: ${this.comChoice}`, 
+                    winner: this.winner
+                }
+                
+                this.logs.unshift(log)
+            }
+        },
+        lifeOfMe: function(newVal) {
+            if(newVal===0){
+                setTimeout(()=>{
+                confirm('안타깝네요. 당신이 패배하였습니다.')
+                this.lifeOfMe = 3
+                this.lifeOfCom = 3
+                this.myChoice = null
+                this.comChoice = null
+                this.winner = null
+                this.logs = []
+                }, 500)
+            }
+        },
+        lifeOfCom: function(newVal) {
+            if(newVal===0){
+                setTimeout(()=>{
+                confirm('축하합니다. 당신이 승리하였습니다.')
+                this.lifeOfMe = 3
+                this.lifeOfCom = 3
+                this.myChoice = null
+                this.comChoice = null
+                this.winner = null
+                this.logs = []
+                }, 500)
             }
         }
     },
     methods: {
         startGame: function (){
+            // 버튼이 보이지  않음
+            this.isSelectable = false
             if(this.myChoice === null){
                 alert('가위 바위 보 중 하나를 선택해주세요.')
             }else{
