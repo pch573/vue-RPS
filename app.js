2c0d0abff759ef0e1b974ce55d10144947ff51e7
pch573 <cksgho573@gmail.com>
2021-09-27 15:22:20 +0900

    리팩토링

--- app.js
+++ app.js
@@ -8,82 +8,64 @@ new Vue({
         lifeOfMe: 3,
         lifeOfCom: 3,
         isSelectable: true,
-        logs:[]
+        logs:[],
+        selects:[
+            { name: '가위', value: 'scissor'},
+            { name: '바위', value: 'rock'},
+            { name: '보', value: 'paper'},
+            
+        ]
+        },
+        computed: {
+            myChoiceImg: function(){
+                return this.myChoice !== null?`images/${this.myChoice}.jpg` :'images/question.jpg'
+            },
+            comChoiceImg: function(){
+                return this.comChoice !== null?`images/${this.comChoice}.jpg` :'images/question.jpg'
+            },
+            leftLifeOfMe: function(){
+                return 3 - this.lifeOfMe
+            },
+            leftLifeOfCom: function(){
+                return 3 - this.lifeOfCom
+            }
         },
         watch: {
             count: function(newVal){
-                if(newVal ===0){
-                let number = Math.random()
-                if(number < 0.33){
-                    this.comChoice = 'scissor'
-                }else if(number <0.66){
-                    this.comChoice = 'rock'    
-                }else{
-                    this.comChoice = 'paper'
-                }
-                //   가위바위보 승패 결정 
-                if(this.myChoice === this.comChoice) this.winner= 'no one'
-                else if(this.myChoice === 'rock' && this.comChoice ==='scissor')this.winner ='me'
-                else if(this.myChoice === 'scissor' && this.comChoice === 'paper')this.winner ='me'
-                else if(this.myChoice === 'paper' && this.comChoice === 'rock')this.winner ='me'
-                else if(this.myChoice === 'scissor' && this.comChoice === 'rock')this.winner ='com'
-                else if(this.myChoice === 'paper' && this.comChoice ==='scissor')this.winner ='com'
-                else if(this.myChoice === 'rock' && this.comChoice ==='paper')this.winner ='com'
-                else this.winner= 'error'
-    
-                // 몫 차감   
-                if(this.winner=== 'me'){
-                     this.lifeOfCom --
-                 }
-                else if(this.winner=== 'com'){
-                    this.lifeOfMe --
-                }   
+            if (newVal === 0) {
+                //  컴퓨터가 가위바위보를 선택하는 
+                this.selectCom()
+                //   가위바위보 승패 결정 & 몫 차감
+                this.whoIsWin()
+
+                // 게임 리셋
                 this.count = 3
-                // 버튼이 다시 보이게 됨
                 this.isSelectable = true
 
-                let log = {
-                    message: `You: ${this.myChoice}, Computer: ${this.comChoice}`, 
-                    winner: this.winner
-                }
-                
-                this.logs.unshift(log)
+                // 로그를 업데이트 
+                this.updateLogs()
             }
         },
         lifeOfMe: function(newVal) {
             if(newVal===0){
-                setTimeout(()=>{
-                confirm('안타깝네요. 당신이 패배하였습니다.')
-                this.lifeOfMe = 3
-                this.lifeOfCom = 3
-                this.myChoice = null
-                this.comChoice = null
-                this.winner = null
-                this.logs = []
-                }, 500)
+                //게임을 종료
+                this.endGame('안타깝네요. 당신이 패배하였습니다.')
             }
         },
         lifeOfCom: function(newVal) {
             if(newVal===0){
-                setTimeout(()=>{
-                confirm('축하합니다. 당신이 승리하였습니다.')
-                this.lifeOfMe = 3
-                this.lifeOfCom = 3
-                this.myChoice = null
-                this.comChoice = null
-                this.winner = null
-                this.logs = []
-                }, 500)
+                this.endGame('축하합니다. 당신이 승리하였습니다.')
             }
         }
     },
     methods: {
         startGame: function (){
-            // 버튼이 보이지  않음
-            this.isSelectable = false
+         
             if(this.myChoice === null){
                 alert('가위 바위 보 중 하나를 선택해주세요.')
             }else{
+                   // 버튼이 보이지  않음
+                this.isSelectable = false
                 let countDown = setInterval(()=>{
                     this.count --
                     if(this.count ===0){
@@ -91,6 +73,52 @@ new Vue({
                     }
                 }, 1000)
             }
+        },
+        selectCom: function (){
+             let number = Math.random()
+                if(number < 0.33){
+                    this.comChoice = 'scissor'
+                }else if(number <0.66){
+                    this.comChoice = 'rock'    
+                }else{
+                    this.comChoice = 'paper'
+                }
+        },
+        whoIsWin: function (){
+            if(this.myChoice === this.comChoice) this.winner= 'no one'
+            else if(this.myChoice === 'rock' && this.comChoice ==='scissor')this.winner ='me'
+            else if(this.myChoice === 'scissor' && this.comChoice === 'paper')this.winner ='me'
+            else if(this.myChoice === 'paper' && this.comChoice === 'rock')this.winner ='me'
+            else if(this.myChoice === 'scissor' && this.comChoice === 'rock')this.winner ='com'
+            else if(this.myChoice === 'paper' && this.comChoice ==='scissor')this.winner ='com'
+            else if(this.myChoice === 'rock' && this.comChoice ==='paper')this.winner ='com'
+            else this.winner= 'error'
+
+            // 몫 차감   
+            if(this.winner=== 'me'){
+                 this.lifeOfCom --
+             }
+            else if(this.winner=== 'com'){
+                this.lifeOfMe --
+            }   
+        },
+        updateLogs: function() {
+            let log = {
+                message: `You: ${this.myChoice}, Computer: ${this.comChoice}`, 
+                winner: this.winner
+            }
+            this.logs.unshift(log)
+        },
+        endGame: function(msg) {
+            setTimeout(()=>{
+                confirm(msg)
+                this.lifeOfMe = 3
+                this.lifeOfCom = 3
+                this.myChoice = null
+                this.comChoice = null
+                this.winner = null
+                this.logs = []
+                }, 500)
         }
     }
 })
